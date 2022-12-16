897c3f98dfc5093e84f74c39b8431ff5d35e358f
KazuoCode <KazuoEdits@gmail.com>
2022-12-17 00:56:52 +0100

    5kyu: Simple Pig Latin
    This kata seems to have an incomplete description since punctuation and
    numbers are also in the tests which dont need "ay" appended to them.
    
    Not sure why the website marked this as incomplete as the code was
    already here, I only rewrote it in a nicer way.

--- Nodejs/5kyu/simple-pig-latin.js
+++ Nodejs/5kyu/simple-pig-latin.js
@@ -1,14 +1,24 @@
-// CodeKata: https://www.codewars.com/kata/520b9d2ad5c005041100000f
+/**
+ * CodeKata: ⁡⁣⁢⁣https://www.codewars.com/kata/520b9d2ad5c005041100000f⁡
+ * Move the first letter of each word to the end of it, then add "ay" to the end of the word.
+ * Leave punctuation marks untouched.
+ *
+ * Examples
+ * pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+ * pigIt('Hello world !');     // elloHay orldway !
+ */
 
 function pigIt(str) {
-    return str.split(' ').map(word => {
-        if (word.match(/([A-Z]|[a-z])/)) {
-            let sliced = word.split('').slice(1);
-            sliced.push(word[0]);
-            sliced.push('a', 'y');
-            return sliced.join('');
-        } else {
-            return word;
-        }
-    }).join(' ');
+  return str
+    .split(' ')
+    .map((word) => {
+      if (!word.match(/([A-Z]|[a-z])/)) return word;
+      word += word.slice(0, 1) + 'ay';
+      return word.substring(1);
+    })
+    .join(' ');
 }
+
+// Codewars provided tests.
+console.log(pigIt('Pig latin is cool') === 'igPay atinlay siay oolcay');
+console.log(pigIt('This is my string') === 'hisTay siay ymay tringsay');
